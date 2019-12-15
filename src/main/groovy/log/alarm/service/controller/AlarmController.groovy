92ff2ac690c5921c5d4e5c706dc062dd83b60257
Hr. Strange <msh@cardlay.com>
2019-12-15 15:26:53 +0100

    update controller and service with new functionality

--- src/main/groovy/log/alarm/service/controller/AlarmController.groovy
+++ src/main/groovy/log/alarm/service/controller/AlarmController.groovy
@@ -6,18 +6,24 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.Patch
+import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.QueryValue
 import io.micronaut.validation.Validated
 import log.alarm.service.domain.model.SaveAlarm
+import log.alarm.service.domain.model.UpdateAlarm
 import log.alarm.service.service.AlarmService
-
+import javax.annotation.Nullable
 import javax.inject.Inject
 import javax.validation.Valid
+import javax.validation.constraints.NotNull
 
 @Validated
 @Controller("/alarms")
 class AlarmController {
 
+    @Inject
     private final AlarmService alarmService
 
     AlarmController(AlarmService service) {
@@ -29,6 +35,9 @@ class AlarmController {
         AlarmService.ServiceResult result = alarmService.saveAlarm(saveAlarm)
 
         switch (result.code) {
+            case 200:
+                return HttpResponse.ok(result.body)
+
             case 201:
                 return HttpResponse.created(result.body)
 
@@ -44,9 +53,56 @@ class AlarmController {
         }
     }
 
-    @Get("/")
-    HttpResponse getAllAlarms() {
-        AlarmService.ServiceResult result = alarmService.getAll()
+    @Get("/{?customer_id,severity,status}")
+    HttpResponse getAllAlarms(@Nullable @QueryValue("customer_id") String customerID,
+                              @Nullable @QueryValue("severity") String severity,
+                              @Nullable @QueryValue("status") String status) {
+        AlarmService.ServiceResult result
+        if (customerID && severity && !status) { result = alarmService.getByCustomerAndSeverity(customerID, severity) }
+        else if (customerID && status && !severity) { result = alarmService.getByCustomerAndStatus(customerID, status) }
+        else if (customerID && !severity && !status) { result = alarmService.getByCustomerID(customerID) }
+        else { result = alarmService.getAll() }
+
+        switch (result.code) {
+            case 200:
+                return HttpResponse.ok(result.body)
+
+            case 400:
+                return HttpResponse.badRequest()
+
+            case 404:
+                return HttpResponse.notFound()
+
+            case 500:
+            default:
+                HttpResponse.serverError()
+        }
+    }
+
+    @Get("/{alarm_id}")
+    HttpResponse getAlarmById(@NotNull @PathVariable(value = "alarm_id") Long alarmID) {
+        AlarmService.ServiceResult result = alarmService.getByAlarmID(alarmID)
+
+        switch (result.code) {
+            case 200:
+                return HttpResponse.ok(result.body)
+
+            case 400:
+                return HttpResponse.badRequest()
+
+            case 404:
+                return HttpResponse.notFound()
+
+            case 500:
+            default:
+                HttpResponse.serverError()
+        }
+    }
+
+    @Patch("/{alarm_id}")
+    HttpResponse updateAlarmByID(@NotNull @PathVariable(value = "alarm_id") Long alarmID,
+                                 @Valid @Body UpdateAlarm updateBody) {
+        AlarmService.ServiceResult result = alarmService.updateAlarmByID(alarmID, updateBody)
 
         switch (result.code) {
             case 200:
--- src/main/groovy/log/alarm/service/service/AlarmService.groovy
+++ src/main/groovy/log/alarm/service/service/AlarmService.groovy
@@ -3,7 +3,11 @@ package log.alarm.service.service
 import grails.gorm.transactions.Transactional
 import log.alarm.service.domain.db.AlarmModel
 import log.alarm.service.domain.model.SaveAlarm
+import log.alarm.service.domain.model.UpdateAlarm
+
 import javax.inject.Singleton
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 @Singleton
 @Transactional
@@ -23,7 +27,7 @@ class AlarmService {
                 return new ServiceResult(code: 500)
             }
             logAlarm.save()
-            return new ServiceResult(code: 201, body: logAlarm.toReturn())
+            return new ServiceResult(code: 200, body: logAlarm.toReturn())
         }
 
         logAlarm = new AlarmModel(
@@ -44,6 +48,43 @@ class AlarmService {
         new ServiceResult(code: 201, body: logAlarm.toReturn())
     }
 
+    ServiceResult getByCustomerAndStatus(@NotNull @NotBlank String customerID, @NotNull @NotBlank String status) {
+        if (!AlarmModel.Status.values().contains(status.toUpperCase())) {
+            return new ServiceResult(code: 400)
+        }
+        AlarmModel.Status stat = AlarmModel.Status.valueOf(status)
+        List<AlarmModel> allFound = AlarmModel.findAllByCustomerIDAndStatus(customerID, stat)
+
+        if(!allFound) {
+            return new ServiceResult(code: 200, body: ["num_results" : allFound.size(), "content" : null])
+        }
+
+        List<Map> returnList = allFound.collect { it.toReturn() }
+        new ServiceResult(code: 200, body: ["num_results" : returnList.size(), "content" : returnList])
+    }
+
+    ServiceResult getByCustomerAndSeverity(@NotNull @NotBlank String customerID, @NotNull @NotBlank String severity) {
+        List<AlarmModel> allFound = AlarmModel.findAllByCustomerIDAndSeverity(customerID, severity)
+
+        if(!allFound) {
+            return new ServiceResult(code: 200, body: ["num_results" : allFound.size(), "content" : null])
+        }
+
+        List<Map> returnList = allFound.collect { it.toReturn() }
+        new ServiceResult(code: 200, body: ["num_results" : returnList.size(), "content" : returnList])
+    }
+
+    ServiceResult getByCustomerID(@NotNull @NotBlank String customerID) {
+        List<AlarmModel> allForCustomer = AlarmModel.findAllByCustomerID(customerID)
+
+        if(!allForCustomer) {
+            return new ServiceResult(code: 200, body: ["num_results" : allForCustomer.size(), "content" : null])
+        }
+
+        List<Map> returnList = allForCustomer.collect { it.toReturn() }
+        new ServiceResult(code: 200, body: ["num_results" : returnList.size(), "content" : returnList])
+    }
+
     ServiceResult getAll() {
         List<AlarmModel> all = AlarmModel.list()
 
@@ -54,6 +95,38 @@ class AlarmService {
         List<Map> returnList = all.collect { it.toReturn() }
 
         new ServiceResult(code: 200, body: ["num_results" : returnList.size(), "content" : returnList])
+    }
+
+    ServiceResult getByAlarmID(@NotNull Long alarmID) {
+        AlarmModel foundAlarm = AlarmModel.findById(alarmID)
+
+        if (!foundAlarm) {
+            return new ServiceResult(code: 404)
+        }
+
+        new ServiceResult(code: 200, body: foundAlarm.toReturn())
+    }
+
+    ServiceResult updateAlarmByID(@NotNull Long alarmID, UpdateAlarm body) {
+        if (!AlarmModel.Status.values().contains(body.status.toUpperCase())) {
+            return new ServiceResult(code: 400)
+        }
+
+        AlarmModel foundAlarm = AlarmModel.findById(alarmID)
+
+        if (!foundAlarm) {
+            return new ServiceResult(code: 404)
+        }
+
+        foundAlarm.status = AlarmModel.Status.valueOf(body.status)
+        foundAlarm.comment = body.comment
+
+        if (foundAlarm.hasErrors()) {
+            return new ServiceResult(code: 500)
+        }
+
+        foundAlarm.save()
+        new ServiceResult(code: 200, body: foundAlarm.toReturn())
     }
 
     static class ServiceResult {
